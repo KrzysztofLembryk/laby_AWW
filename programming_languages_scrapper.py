e32e364a7f23c675d9cfcda1049466c237fee73d
KrzysztofLembryk <krzysztof.lembryk@gmail.com>
2024-03-13 08:36:15 +0100

    removed permalink - didnt work

--- programming_languages_scrapper.py
+++ programming_languages_scrapper.py
@@ -32,8 +32,6 @@ def init_markdown(name: str, draft: str, layout: str):
     markdown += "title: " + name + "\n"
     markdown += "draft: " + draft + "\n"
     markdown += "layout: " + layout + "\n"
-    if layout == "home":
-        markdown += "permalink: https://krzysztoflembryk.github.io/\n"
     markdown += "---\n"
 
     return markdown
