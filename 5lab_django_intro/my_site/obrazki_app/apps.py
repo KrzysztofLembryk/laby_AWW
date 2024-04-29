cdeeeaaa5f694dd221b0e9dca84d0f9518f8a12a
KrzysztofLembryk <krzysztof.lembryk@gmail.com>
2024-04-29 18:32:43 +0200

    ended polls impl, started obrazki_app

---
+++ 5lab_django_intro/my_site/obrazki_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ObrazkiAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'obrazki_app'
--- 5lab_django_intro/my_site/polls/views.py
+++ 5lab_django_intro/my_site/polls/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Question
-from django.template import loader
-from django.http import Http404
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.db.models import F
 
+from .models import Question, Choice
 
+# Home page view
+def home_view(request):
+    return render(request, "polls/home_view.html")
 
 # Create your views here.
 def index(request):
@@ -31,7 +33,33 @@ def detail(request, question_id):
     return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
-    return HttpResponse(f"You're looking at the results of question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}")
+
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+
+    except (KeyError, Choice.DoesNotExist):
+
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "You didn't select a choice.",
+        })
+
+    selected_choice.votes = F("votes") + 1
+    selected_choice.save()
+
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+
+
+
+
+
+
+
+
