230f9654497d308d3b1a8b24d7db076b88047436
KrzysztofLembryk <krzysztof.lembryk@gmail.com>
2024-04-29 19:51:36 +0200

    added part of login and logout page

--- 5lab_django_intro/my_site/obrazki_app/views.py
+++ 5lab_django_intro/my_site/obrazki_app/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 def index(request):
     return render(request, 'obrazki_app/index.html')
+
+def login_view(request):
+    if request.method == "POST":
+
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            return redirect('index')
+        else:
+            return render(request, 'obrazki_app/login.html', {'error': 'Niepoprawne dane'})
+    else:
+        return render(request, 'obrazki_app/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
