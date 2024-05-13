e6a7e0f52fc5c1542b4c22e9546fa4ccf99e4bef
KrzysztofLembryk <krzysztof.lembryk@gmail.com>
2024-05-13 18:12:55 +0200

    added get randImg in fastApi that returns random image from database, or generates random big image or throws errro

--- 8lab_TypeScript_tutorial/serverFastApi/serverAPI.py
+++ 8lab_TypeScript_tutorial/serverFastApi/serverAPI.py
@@ -6,6 +6,9 @@ from sqlalchemy.orm import Session, sessionmaker, joinedload
 from typing import List, Dict
 import json
 from . import models
+import random
+import time
+from fastapi import HTTPException
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
@@ -30,3 +33,56 @@ async def save(data_json: Dict, db: Session = Depends(get_db)):
 async def img_lst(db: Session = Depends(get_db)):
     images = db.query(models.SvgImage).all()
     return {image.name:{"width": image.width, "height": image.height, "rects": json.loads(image.svg)}  for image in images}
+
+def too_big_svg():
+    svg = {}
+    svg["width"] = 350
+    svg["height"] = 350
+    svg["rects"] = []
+
+    for i in range(0, random.randint(800, 1500)):
+        x1 = random.randint(0, 298)
+        y1 = random.randint(0, 298)
+        x2 = random.randint(x1 + 1, 300)
+        y2 = random.randint(y1 + 1, 300)
+        width = x2 - x1
+        height = y2 - y1
+        color = "#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
+        svg["rects"].append(
+                            {
+                                "x": x1,
+                                "y": y1,
+                                "width": width,
+                                "height": height,
+                                "color": color
+                            })
+    return svg
+
+def ret_random_good_svg(images):
+    random_number = random.randint(0, len(images) - 1)
+    image = images[random_number]
+    return {image.name:{"width": image.width, "height": image.height, "rects": json.loads(image.svg)}}
+
+def lengthy_generating_svg(images):
+    time.sleep(5)
+    return ret_random_good_svg(images)
+
+def ret_500_error():
+    raise HTTPException(status_code=500, detail="Internal Server Error - happened when generating random svg image.")
+
+
+
+@app.get("/randomImg")
+async def random_img(db: Session = Depends(get_db)):
+    images = db.query(models.SvgImage).all()
+    random_number = random.randint(0, 4)
+
+    if (random_number == 0):
+        return json.dumps(too_big_svg())
+    elif (random_number == 1):
+        return json.dumps(lengthy_generating_svg(images))
+    elif (random_number == 2):
+        return ret_500_error()
+    else:
+        # Rozpatrzyc przypadek gdy zero svg obrazkow
+        return json.dumps(ret_random_good_svg(images))
